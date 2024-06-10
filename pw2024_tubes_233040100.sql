03e431e11263dc43daf61f01b419d77c928db509
metacantikadewi <metacantika130506@gmail.com>
2024-06-10 21:02:03 +0700

    ngejain tugas

--- pw2024_tubes_233040100.sql
+++ pw2024_tubes_233040100.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 21, 2024 at 01:22 AM
+-- Generation Time: Jun 10, 2024 at 01:59 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,43 +24,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `teknologi`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `teknologi` (
-  `id` int NOT NULL,
-  `nama_produk` varchar(10) NOT NULL,
-  `description` varchar(255) NOT NULL
+CREATE TABLE `product` (
+  `produk_id` int NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga_produk` varchar(255) NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `teknologi`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `teknologi` (`id`, `nama_produk`, `description`) VALUES
-(1, 'MACBOOK  ', 'Macbook Air 13 \r\nKapasitas Penyimpanan 512 GB \r\nMemori Standar 8 GB \r\n\r\n'),
-(2, 'MACBOOK  ', 'MACBOOK PRO\r\nWarna Macbook Silver Dan Space Grey\r\nMemori Standar 16 GB Atau 24GB\r\nMacbook Pro Dikeluarkan Pada Tahun 2022\r\n\r\n\r\n');
+INSERT INTO `product` (`produk_id`, `nama_produk`, `harga_produk`, `image`) VALUES
+(9, 'iphone 12 pro max', '13.000.000', '6666ac64c3c7c.jpeg'),
+(12, 'iphone 15 pro max ', '32.000.000', '6666a29e04a4b.jpeg'),
+(14, 'apdator iphone ', '479.000', '6666a59997ac8.jpeg'),
+(15, 'kabel charger iphone ', '449.000', '6666a5ca295d0.jpeg'),
+(16, 'iphone 13 pro max', '18.249.000 ', '6666ad9e610ff.jpeg'),
+(18, 'iphone 14 pro max', '18.249.000 ', '66670302ccbac.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, 'admin', '$1$gq4b6CAm$kauj41pY7.o5RQ9mk/.cT.', 'metacantika130506@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `teknologi`
+-- Indexes for table `product`
 --
-ALTER TABLE `teknologi`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`produk_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `teknologi`
+-- AUTO_INCREMENT for table `product`
 --
-ALTER TABLE `teknologi`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `product`
+  MODIFY `produk_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
